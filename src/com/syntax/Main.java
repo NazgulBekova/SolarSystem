ae2347085a998c392dab77a6389b8a768ded9af8
NazgulBekova <dairovna.n@gmail.com>
2022-05-17 19:17:42 -0400

    Solar System Basic Project ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,13 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem factorX=new SolarSystem();
+        factorX.sun="1";
+        factorX.moon="4";
+        factorX.planets="2";
+        factorX.star="100";
+    }
+}
