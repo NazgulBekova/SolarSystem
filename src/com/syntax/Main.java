96cbd8e8e07b7e86dd3045c76fdbaaf469195156
NazgulBekova <dairovna.n@gmail.com>
2022-05-17 19:31:45 -0400

    feature Sun added successfully

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -9,5 +9,11 @@ public class Main {
         factorX.moon="4";
         factorX.planets="2";
         factorX.star="100";
+
+        //adding features to our sun
+        FeatureSun Xfact=new FeatureSun();
+        Xfact.color="green";
+        Xfact.radius="1000000000";
+        Xfact.heat="30215454545";
     }
 }
